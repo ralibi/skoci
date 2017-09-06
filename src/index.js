9995acc1b88d91b76878ef66c385d7a7ef73ee39
raymondralibi <ralibi@starqle.com>
2017-09-06 14:58:08 +0700

    Fix unnecessary `/` char

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ function getCycle () {
 // Return url appended with an `action`
 // All `:pattern` in `url` will be replace by `params['pattern']`
 function getUrl (url, params, action) {
-  let resultUrl = url.replace(/\:([a-zA-Z_]+)/g, function (match, offset) { return params[offset] ? params[offset] : '' })
+  let resultUrl = url.replace(/:([a-zA-Z_]+)/g, function (match, offset) { return params[offset] ? params[offset] : '' })
 
   // replace `abc/.ext` to `abc.ext`
   resultUrl = resultUrl.replace(/\/\./, '.')
